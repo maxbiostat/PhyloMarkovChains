448ddf0ca41bbe1f635b4caea5f339f3f0cb86d2
Luiz Max Carvalho <luizepidemiologia@gmail.com>
2022-05-24 22:36:59 -0300

    Getting rid of labels

--- R/clade_utils.r
+++ R/clade_utils.r
@@ -7,13 +7,12 @@ clade_indicators <- function(ind, L){
 #' Get the clades in a tree
 #'
 #' @param tree a phylo object
-#' @param labels labels for the tips
 #'
 #' @return a vector with the 2n-1 clades
 #' @export get_clades
 #'
-get_clades <- function(tree, labels){
-  splits <- phangorn::as.splits(tree, labels = labels)
+get_clades <- function(tree){
+  splits <- phangorn::as.splits(tree)
   sorted <- sort(tree$tip.label)
   label.orders <- sapply(tree$tip.label, function(y) match(y, sorted))
   ordered.splits <- sapply(splits, function(x) label.orders[x])
